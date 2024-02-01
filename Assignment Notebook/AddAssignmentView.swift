d3729a1716d0f045b33c033beb5869a05935dc5f
Tom Bredemeier <tbredemeier@barrington220.org>
2024-02-01 11:52:57 -0600

    Completed AddAssignmentView

---
+++ Assignment Notebook/AddAssignmentView.swift
@@ -0,0 +1,18 @@
+//
+//  AddAssignmentView.swift
+//  Assignment Notebook
+//
+//  Created by Tom Bredemeier on 2/1/24.
+//
+
+import SwiftUI
+
+struct AddAssignmentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddAssignmentView()
+}
