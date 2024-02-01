9feca32ec10ff905a3a7bc1aa3fda1d4ebe3d253
Tom Bredemeier <tbredemeier@barrington220.org>
2024-02-01 11:09:21 -0600

    Added AssignmentItem

--- Assignment Notebook/ContentView.swift
+++ Assignment Notebook/ContentView.swift
@@ -8,17 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var assignmentItems = [AssignmentItem(course: "Math", description: "Unit Test", date: Date()),
+    AssignmentItem(course: "English", description: "Term Paper", date: Date())]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                ForEach(assignmentItems) {item in
+                    HStack {
+                        VStack {
+                            Text(item.course)
+                                .font(.headline)
+                            Text(item.description)
+                        }
+                        Spacer()
+                        Text(item.date, style: .date)
+                    }
+                }
+                .onMove(perform: { indices, newOffset in
+                    assignmentItems.move(fromOffsets: indices, toOffset: newOffset)
+                })
+                .onDelete(perform: { indexSet in
+                    assignmentItems.remove(atOffsets: indexSet)
+                })
+            }
+            .navigationBarTitle("Assignments", displayMode: .inline)
+            .navigationBarItems(leading: EditButton())
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct AssignmentItem: Identifiable {
+    var id = UUID()
+    var course = String()
+    var description = String()
+    var date = Date()
 }
