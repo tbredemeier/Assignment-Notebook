1483b00b75c805d6986da0c7b8ea11c9340ed179
Tom Bredemeier <tbredemeier@barrington220.org>
2024-02-01 10:16:52 -0600

    Initial Commit

---
+++ Assignment Notebook/Assignment_NotebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_NotebookApp.swift
+//  Assignment Notebook
+//
+//  Created by Tom Bredemeier on 2/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_NotebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
