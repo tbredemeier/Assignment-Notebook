50e87ec1623583b9aee61670a69a0cc8c4b5f2ca
Tom Bredemeier <tbredemeier@barrington220.org>
2026-01-27 10:45:17 -0600

    Set up list edit functionality, Created initial To-Do list, Redesigned list display

--- Assignment Notebook/ContentView.swift
+++ Assignment Notebook/ContentView.swift
@@ -8,17 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var assignmentItems =
+    [AssignmentItem(course: "Algebra", description: "Linear Equation", dueDate: Date()),
+     AssignmentItem(course: "History", description: "Civil War Paper", dueDate: Date()),
+     AssignmentItem(course: "Science", description: "Atomic Bomb Lab", dueDate: Date())]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                ForEach(assignmentItems) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.course)
+                                .font(.headline)
+                            Text(item.description)
+                                .font(.subheadline)
+                                .foregroundColor(.secondary)
+                        }
+                        Spacer()
+                        Text(item.dueDate, style: .date)
+                    }
+                }
+                .onMove { indices, newOffset in
+                    assignmentItems.move(fromOffsets: indices, toOffset: newOffset)
+                }
+                .onDelete(perform: { indexSet in
+                    assignmentItems.remove(atOffsets: indexSet)
+                })
+            }
+            .navigationBarTitle("Assignment Notebook", displayMode: .inline)
+            .navigationBarItems(leading: EditButton())
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct AssignmentItem: Identifiable {
+    var id = UUID()
+    var course = String()
+    var description = String()
+    var dueDate = Date()
 }
